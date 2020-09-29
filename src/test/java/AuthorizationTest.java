6a96446653fbfe1aebc81a2974c23e80197db4b2
marym <maria_manzhos@tut.by>
2020-09-29 17:58:37 +0300

    add items name and prices

--- src/test/java/AuthorizationTest.java
+++ src/test/java/AuthorizationTest.java
@@ -2,6 +2,9 @@ import Authorization.Authorization;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
+
+import java.util.List;
+
 import static org.testng.Assert.*;
 
 public class AuthorizationTest extends Authorization {
@@ -11,6 +14,17 @@ public class AuthorizationTest extends Authorization {
         WebElement titleProducts = driver.findElement(By.className("product_label"));
         String pageProducts = titleProducts.getText();
         assertEquals(pageProducts,"Products");
+        List<WebElement> items = driver.findElements(By.className("inventory_item_name"));
+        List<WebElement> prices = driver.findElements(By.className("inventory_item_price"));
+
+        for (int i = 0; i < items.size(); i++){
+            System.out.println(items.get(i).getText());
+        }
+
+        for (int i = 0; i < prices.size(); i++){
+            System.out.println(prices.get(i).getText());
+        }
+
         logOut();
     }
 }
