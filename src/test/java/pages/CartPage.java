44942e5b70e5e84c0998cb04f1b106e876ca4106
marym <maria_manzhos@tut.by>
2020-10-10 11:45:21 +0300

    add Waiters add test productsShouldBeRemovedFromCart

--- src/test/java/pages/CartPage.java
+++ src/test/java/pages/CartPage.java
@@ -1,14 +1,18 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.util.concurrent.TimeUnit;
 
 import static org.testng.Assert.assertEquals;
 
 public class CartPage extends BasePage {
 
     public static final By CHECKOUT_BUTTON = By.className("checkout_button");
+    public static final By ITEMS = By.className("cart_item");
     String priceLocator = "//*[contains(text(),'%s')]/ancestor::*[@class='cart_item']" +
             "//div[@class='inventory_item_price']";
     String quantityLocator = "//*[contains(text(),'%s')]/ancestor::*[@class='cart_item']" +
@@ -27,6 +31,16 @@ public class CartPage extends BasePage {
         String actualQuantity = driver.findElement(By.xpath(String.format(quantityLocator, productName))).getText();
         assertEquals(actualPrice, price, "Price is not correct");
         assertEquals(actualQuantity, quantity, "Price is not correct");
+    }
+
+    public boolean isCartEmpty() {
+        try{
+            driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+            driver.findElement(ITEMS);
+            return false;
+        } catch (NoSuchElementException e){
+            return true;
+        }
     }
 
     public void goToCheckoutInformationPage() {
--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -12,13 +12,13 @@ import java.util.concurrent.TimeUnit;
 public class BaseTest {
 
     WebDriver driver;
-    //LoginPage loginPage;
-    LoginPageFactory loginPage;
+    LoginPage loginPage;
     ProductsPage productsPage;
     CartPage cartPage;
     Header header;
     CheckoutInformationPage checkoutInformationPage;
     CheckoutOverviewPage checkoutOverviewPage;
+    ProductDetailPage productDetailPage;
 
     @BeforeMethod
     public void setUp() {
@@ -28,12 +28,13 @@ public class BaseTest {
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-        loginPage = new LoginPageFactory(driver);
+        loginPage = new LoginPage(driver);
         productsPage = new ProductsPage(driver);
         cartPage = new CartPage(driver);
         header = new Header(driver);
         checkoutInformationPage = new CheckoutInformationPage(driver);
         checkoutOverviewPage = new CheckoutOverviewPage(driver);
+        productDetailPage = new ProductDetailPage(driver);
     }
 
     @AfterMethod
