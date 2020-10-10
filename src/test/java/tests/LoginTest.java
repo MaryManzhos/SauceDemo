65c147fce9c917902abef7198cde94edfc1734df
marym <maria_manzhos@tut.by>
2020-10-10 11:43:58 +0300

    add Waiters

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -11,6 +11,7 @@ public class LoginTest extends BaseTest {
     @Test
     public void isSuccessfulAuthorization() {
         loginPage.openPage();
+        loginPage.isPageOpen();
         loginPage.logIn(USERNAME_1, PASSWORD);
         assertEquals(productsPage.getNameOfPage(), "Products", "Is not page Products");
     }
@@ -18,6 +19,7 @@ public class LoginTest extends BaseTest {
     @Test
     public void displayErrorMessageWhenUsernameIsEmpty() {
         loginPage.openPage();
+        loginPage.isPageOpen();
         loginPage.logIn("", PASSWORD);
         assertEquals(loginPage.getErrorMessage(), "Epic sadface: Username is required", "Don't display error message");
     }
@@ -25,6 +27,7 @@ public class LoginTest extends BaseTest {
     @Test
     public void displayErrorMessageWhenPasswordIsEmpty() {
         loginPage.openPage();
+        loginPage.isPageOpen();
         loginPage.logIn(USERNAME_1, "");
         assertEquals(loginPage.getErrorMessage(), "Epic sadface: Password is required", "Don't display error message");
     }
@@ -32,6 +35,7 @@ public class LoginTest extends BaseTest {
     @Test
     public void displayErrorMessageWhenUsernameIsNotValid() {
         loginPage.openPage();
+        loginPage.isPageOpen();
         loginPage.logIn(USERNAME_1, "12345");
         assertEquals(loginPage.getErrorMessage(), "Epic sadface: Username and password do not match any user in this service", "Don't display error message");
     }
