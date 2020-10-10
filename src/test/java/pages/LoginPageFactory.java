e6f3439daac624165350a8d0432081179ab41ceb
marym <maria_manzhos@tut.by>
2020-10-10 18:36:43 +0300

    add Pattern PageFactory

---
+++ src/test/java/pages/LoginPageFactory.java
@@ -0,0 +1,43 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import static testData.TestData.URL;
+
+public class LoginPageFactory extends BasePage{
+    @FindBy(id = "user-name")
+    WebElement usernameInput;
+    @FindBy(id = "password")
+    WebElement passwordInput;
+    @FindBy(id = "login-button")
+    WebElement loginButton;
+    @FindBy(css = "[data-test=error]")
+    WebElement errorMessage;
+
+    public LoginPageFactory(WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(driver,this);
+    }
+
+    public void isPageOpen() {
+        wait.until(ExpectedConditions.visibilityOf(loginButton));
+    }
+
+    public void openPage() {
+        driver.get(URL);
+    }
+
+    public void logIn(String username, String password) {
+        usernameInput.sendKeys(username);
+        passwordInput.sendKeys(password);
+        loginButton.click();
+    }
+
+    public String getErrorMessage() {
+        return errorMessage.getText();
+    }
+}
--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -12,7 +12,8 @@ import java.util.concurrent.TimeUnit;
 public class BaseTest {
 
     WebDriver driver;
-    LoginPage loginPage;
+    //LoginPage loginPage;
+    LoginPageFactory loginPage;
     ProductsPage productsPage;
     CartPage cartPage;
     Header header;
@@ -29,7 +30,7 @@ public class BaseTest {
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-        loginPage = new LoginPage(driver);
+        loginPage = new LoginPageFactory(driver);
         productsPage = new ProductsPage(driver);
         cartPage = new CartPage(driver);
         header = new Header(driver);
